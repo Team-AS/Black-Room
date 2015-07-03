2e1b4f486da6a4da3c1822dbbcd9f9cdfb15735f
Duduzzing <juhyung0109gmail.com>
2015-07-04 00:31:52 +0900

    code update

--- BlackRoom.js
+++ BlackRoom.js
@@ -13,9 +13,13 @@ var CTX = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
 
 var PECTX = CTX.createPackageContext("com.mojang.minecraftpe", android.content.Context.CONTEXT_IGNORE_SECURITY);
 
-var screenWidth = CTX.getWindowManager().getDefaultDisplay().getWidth();
+var tempScreenWidth = CTX.getWindowManager().getDefaultDisplay().getWidth();
 
-var screenHeight = CTX.getWindowManager().getDefaultDisplay().getHeight();
+var tempScreenHeight = CTX.getWindowManager().getDefaultDisplay().getHeight();
+
+var screenWidth = Math.max( tempScreenWidth, tempScreenHeight);
+
+var screenHeight = Math.min( tempScreenWidth, tempScreenHeight);
 
 var FONT_PATH = SDCARD + "/Duduzzing/Survival-Kit/minecraft.ttf";
 
@@ -35,6 +39,8 @@ var Gravity = android.view.Gravity;
 
 var GridLayout = android.widget.GridLayout;
 
+var ImageView = android.widget.ImageView;
+
 var LinearLayout = android.widget.LinearLayout;
 
 var PopupWindow = android.widget.PopupWindow;
@@ -44,6 +50,13 @@ var Space = android.widget.Space;
 var TextView = android.widget.TextView;
 
 var View = android.view.View;
+
+
+var peAssets = PECTX.getAssets();
+
+var spritesheet = new BitmapFactory.decodeStream(peAssets.open("images/gui/spritesheet.png"));
+
+
 
 var gui = {};
 
@@ -63,6 +76,106 @@ gui.ninepatch = function(file) {
 var blackRoom = {};
 
 /**
+
+ClientMessage for BlackRoom script
+
+String message
+-may not be string though
+
+*/
+
+blackRoom.message = function(message) {
+
+  clientMessage("<Black Room> " + message);
+
+};
+
+blackRoom.download = function(url, path) {
+  new java.lang.Thread(new java.lang.Runnable({
+    run: function() {
+      try {
+        var file = new java.io.File(SDCARD, path);
+        if (file.exists()) {
+
+          return;
+
+        }
+
+        if (!file.getParentFile().exists()) {
+
+          file.getParentFile().mkdirs();
+
+        }
+
+        var URL = new java.net.URL(url);
+        var urlConn = URL.openConnection();
+        urlConn.connect();
+        var total = urlConn.getContentLength();
+        var bis = new java.io.BufferedInputStream(URL.openStream());
+        var bos = new java.io.BufferedOutputStream(new java.io.FileOutputStream(file));
+
+        var downloadDialog;
+
+        CTX.runOnUiThread(new java.lang.Runnable({
+          run: function() {
+            try {
+
+              downloadDialog = new android.app.ProgressDialog(CTX);
+
+              downloadDialog.setProgressStyle(android.app.ProgressDialog.STYLE_HORIZONTAL);
+              downloadDialog.setMax(total);
+              downloadDialog.setTitle("Resource file downloading");
+              downloadDialog.setMessage("downloading..");
+              downloadDialog.setCancelable(true);
+              downloadDialog.show();
+            } catch(e) {
+              print(e);
+              blackRoom.error(e);
+            }
+
+          }
+        }));
+
+        var data = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
+        var count;
+        var downloadProgress = 0;
+
+        while ((count = bis.read(data)) != -1) {
+          java.lang.Thread.sleep(1);
+          bos.write(data, 0, count);
+          downloadProgress += count;
+
+          CTX.runOnUiThread(new java.lang.Runnable({
+            run: function() {
+              downloadDialog.setProgress(downloadProgress);
+            }
+          }));
+        }
+        urlConn.disconnect();
+        bos.flush();
+        bos.close();
+        bis.close();
+        CTX.runOnUiThread(new java.lang.Runnable({
+          run: function() {
+            try {
+              downloadDialog.dismiss();
+            } catch(e) {
+              print(e);
+              blackRoom.error(e);
+            }
+          }
+        }));
+
+        blackRoom.message(file.getName() + " download finished");
+      } catch(e) {
+        print(e);
+        blackRoom.error(e);
+      }
+    }
+  })).start();
+};
+
+/**
 String name
 -the character's name
 
@@ -70,35 +183,75 @@ return bitmap
 */
 
 blackRoom.getFaceByName = function(name) {
+  var path = SDCARD + "/Download/몹/" + name + ".png";
 
-  return BitmapFactory.decodeFile(SDCARD + "/Download/몹/" + name + ".png");
+  if (java.io.File(path).exists()) {
+
+    return BitmapFactory.decodeFile(path);
+
+  } else {
+
+    throw new Error("No image exists in: " + path);
+
+  }
 
 };
 
 /**
-Show the error in clientMessage
+Show the error in a dialog
 use in try-catch
 */
 
 blackRoom.error = function(e) {
-
-  clientMessage(ChatColor.RED + "<error> " + e + "\nline : " + e.lineNumber);
-
+	 
+	 CTX.runOnUiThread(new java.lang.Runnable( {
+		run: function(){
+			try{    
+  var dialog = new android.app.AlertDialog.Builder(CTX);
+  
+		var text = new EditText(CTX);
+		
+		text.setText(e + "\nLineNumber : " + e.lineNumber);
+		
+		var scroll = new android.widget.ScrollView(CTX);
+		
+		scroll.addView(text);
+		
+		dialog.setTitle("<Black Room> Error!");
+		
+		dialog.setView(scroll);
+		
+		dialog.setNegativeButton("Exit", null);
+		
+		dialog.setPositiveButton("Copy to clipboard",new android.content.DialogInterface.OnClickListener({onClick:function(){
+			
+			 CTX.getSystemService(android.content.Context.CLIPBOARD_SERVICE).setText(text.getText());
+			 
+			 android.widget.Toast.makeText(CTX,"Copied to clipboard", android.widget.Toast.LENGTH_LONG).show(); 
+			 			
+	 }}));
+		dialog.create();
+		dialog.show();
+   }catch(e){
+   	
+   	print(e);
+    }
+    }}));
 };
 
 /**
 
-int milliSec
+int howLong
 -set how long will it vibrate in millisecond
 
 */
 
-blackRoom.vibrate = function(milliSec) {
+blackRoom.vibrate = function(howLong) {
 
   CTX.runOnUiThread(new java.lang.Runnable({
     run: function() {
       try {
-        CTX.getSystemService(android.content.Context.VIBRATOR_SERVICE).vibrate(milliSec);
+        CTX.getSystemService(android.content.Context.VIBRATOR_SERVICE).vibrate(howLong);
 
       } catch(e) {
         blackRoom.error(e);
@@ -113,33 +266,114 @@ blackRoom.vibrate = function(milliSec) {
 
 Prepare PopupWindow for chat
 
-return PopupWindow
+return PopupWindow (obj)
 
 */
 
 blackRoom.prepareChat = function() {
 
+try{
+
   ModPE.setGameSpeed(10);
+  
+ var imageView1 = new ImageView(CTX);
 
-  var talkLayout = new LinearLayout(CTX);
+  var personWindow1 = new PopupWindow(imageView1, screenWidth / 4, screenHeight / 2);
+  personWindow1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
 
-  var talkWindow = new PopupWindow(screenWidth, screenHeight / 3);
+  personWindow1.showAtLocation(CTX.getWindow().getDecorView(), Gravity.LEFT | Gravity.CENTER, 0, 0);
 
-  talkWindow.setContentView(talkLayout);
+  var imageView2 = new ImageView(CTX);
+
+  var personWindow2 = new PopupWindow(imageView2, screenWidth / 4, screenHeight / 2);
+  personWindow2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+
+  personWindow2.showAtLocation(CTX.getWindow().getDecorView(), Gravity.RIGHT | Gravity.CENTER, 0, 0);
+ 
+  
+  var nameBitmap = BitmapFactory.decodeFile(SDCARD+"/Download/talking_name.9.png");
+  
+  var nWid = nameBitmap.getWidth();
+  var nHei = nameBitmap.getHeight();
+
+  var name1 = new Button(CTX);
+  
+  name1.setTextSize(20);
+  
+  name1.setTextColor(Color.BLACK);
+
+  name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+  
+  var nameWindow1 = new PopupWindow(name1, nWid, nHei);
+  
+  nameWindow1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+  
+  nameWindow1.showAtLocation(CTX.getWindow().getDecorView(), Gravity.LEFT | Gravity.CENTER, 0, 70);
+
+  var name2 = new Button(CTX);
+  
+  name2.setTextSize(20)
+  
+  name2.setTextColor(Color.BLACK);
+
+  name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+    
+  var nameWindow2 = new PopupWindow(name2, nWid, nHei);
+  
+  nameWindow2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+  
+  nameWindow2.showAtLocation(CTX.getWindow().getDecorView(), Gravity.RIGHT | Gravity.CENTER, 0, 70);
+
+
+  var screen = new Button(CTX);
+  
+  screen.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+
+  var talkWindow = new PopupWindow(screen,screenWidth, screenHeight / 3);
 
   talkWindow.setBackgroundDrawable(gui.ninepatch("/Download/talking.9.png"));
 
   talkWindow.showAtLocation(CTX.getWindow().getDecorView(), Gravity.CENTER | Gravity.BOTTOM, 0, 0);
 
-  return talkWindow;
+  
+  var arrowBitmap = new Bitmap.createScaledBitmap( new Bitmap.createBitmap(spritesheet,38,11,8,4), 32, 14, false);
+  
+  var arrowWindow = new PopupWindow(32*2,14*2);
+  
+  arrowWindow.setBackgroundDrawable(new Drawable.ColorDrawable(Color.WHITE));
 
+  //arrowWindow.setBackgroundDrawable(new Drawable.BitmapDrawable(arrowBitmap));
+
+  arrowWindow.showAtLocation(CTX.getWindow().getDecorView(), Gravity.CENTER | Gravity.TOP, -20, 0);
+
+
+  var obj = {
+
+    talkWindow: talkWindow,
+    nameWindow1: nameWindow1,
+    nameWindow2: nameWindow2,
+    personWindow1: personWindow1,
+    personWindow2: personWindow2,
+    arrowWindow: arrowWindow
+
+  };
+
+  return obj;
+} catch(e) {
+	
+  blackRoom.error(e);	
+	
+}
 }
 
 /**
 
 the Chatting function
 
-Character[] character
+Object talkWin
+-prepareChat
+
+String[] character
 -defines which character is speaking
 
 Sring[] message
@@ -154,7 +388,7 @@ Bool[] doVibrate
 Bool[] isLeft
 -true = face is on the left side
 
-function[] endFunc
+function[]/null endFunc
 -function that execute when the talking is done
 
 int count
@@ -167,27 +401,26 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
 
   try {
 
+    var person1 = talkWin.personWindow1.getContentView();
+
+    var person2 = talkWin.personWindow2.getContentView();
+    
+    var name1 = talkWin.nameWindow1.getContentView();
+   
+    var name2 = talkWin.nameWindow2.getContentView();
+    
     var letter = message[count].split("");
     var currentChat = letter[0];
     var letterCount = 0;
     var isChatOver = false;
 
-    var talkLayout = talkWin.getContentView();
-
-    talkLayout.removeAllViews();
-
-    ModPE.setGameSpeed(10);
-
-    var screen = new Button(CTX);
+    var screen = talkWin.talkWindow.getContentView();
 
     screen.setText(currentChat);
 
     screen.setGravity(Gravity.LEFT);
 
-    screen.setBackgroundDrawable(new Drawable.ColorDrawable(Color.RED
-    /*TRANSPARENT*/
-
-    ));
+    screen.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
 
     screen.setShadowLayer(1 / Math.pow(10, 10), 4, 4, Color.parseColor("#424142"));
 
@@ -198,33 +431,45 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
     if (color[count] != null) {
       screen.setTextColor(color[count]);
     }
-
-    var params = new LinearLayout.LayoutParams(talkWin.getWidth() * (3 / 4), talkWin.getHeight());
-
-    screen.setLayoutParams(params);
-
-    var face = new Button(CTX);
-
+    
     var faceFile = blackRoom.getFaceByName(character[count]);
 
-    var bit = new Bitmap.createScaledBitmap(
-    faceFile, screenWidth / 5, screenHeight / 4, false);
-
-    face.setBackgroundDrawable(new Drawable.BitmapDrawable(bit));
-
-    var params2 = new LinearLayout.LayoutParams(talkWin.getWidth() / 4, talkWin.getHeight());
-
-    face.setLayoutParams(params2);
+    var face = new Bitmap.createScaledBitmap(
+    faceFile, screenWidth / 5, screenHeight / 3, false);
 
     if (isLeft[count]) {
+    	
+    	 var matrix = new android.graphics.Matrix();
+        matrix.setScale(-1, 1);
+        
+        face = Bitmap.createBitmap(face, 0, 0, face.getWidth(), face.getHeight(), matrix, false);
 
-      talkLayout.addView(face);
-      talkLayout.addView(screen);
+      person2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+
+      person1.setBackgroundDrawable(new Drawable.BitmapDrawable(face));
+      
+      name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+      
+      name2.setText("");
+      
+      name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.parseColor("#b0acad"))); 
+      
+      name1.setText(character[count]); 
+            
 
     } else {
 
-      talkLayout.addView(screen);
-      talkLayout.addView(face);
+      person1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+
+      person2.setBackgroundDrawable(new Drawable.BitmapDrawable(face));
+      
+      name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
+      
+      name1.setText("");
+            
+      name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.parseColor("#b0acad"))); 
+      
+      name2.setText(character[count]);
 
     }
 
@@ -234,14 +479,14 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
 
     }
 
-    //어... 다른 쓰레드에서 셋 텍스트하면 오류가... 
+    var haveClicked = false;
 
     new java.lang.Thread(new java.lang.Runnable({
       run: function() {
 
         try {
 
-          while (isChatOver == false && letterCount < letter.length - 1) {
+          while (!haveClicked && !isChatOver && letterCount < letter.length - 1) {
 
             java.lang.Thread.sleep(50);
 
@@ -252,13 +497,14 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
             CTX.runOnUiThread(new java.lang.Runnable({
               run: function() {
 
-                screen.setText(currentChat);
+                (!isChatOver) ? screen.setText(currentChat) : screen.setText(message[count]);
               }
             }));
 
             if (letterCount == letter.length - 1) {
 
               isChatOver = true;
+              haveClicked = true;
 
             }
 
@@ -278,7 +524,14 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
           isChatOver = true;
 
           screen.setText(message[count]);
+          
+          if (!haveClicked) {
 
+            haveClicked = true;
+            return;
+
+          }
+          
           if (endFunc[count] != null) {
 
             endFunc[count]();
@@ -296,7 +549,13 @@ blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doV
 
             ModPE.setGameSpeed(20);
 
-            talkWin.dismiss();
+            talkWin.talkWindow.dismiss();
+            
+            talkWin.arrowWindow.dismiss();
+            talkWin.nameWindow1.dismiss();
+            talkWin.nameWindow2.dismiss();
+            talkWin.personWindow1.dismiss();
+            talkWin.personWindow2.dismiss();
 
             isChatOver = false;
 
@@ -322,13 +581,13 @@ function useItem(x, y, z, I, b) {
 
     var cha = ["steve", "Slime", "steve", "Slime"];
 
-    var point = [20, 25, 10, 25];
+    var point = [20, 25, 16, 25];
 
-    var doVibrate = [false, false, false, true];
+    var doVibrate = [false, false, false, false];
 
     var isLeft = [true, false, true, false];
 
-    var color = [Color.BLACK, Color.WHITE, Color.YELLOW, Color.BLUE];
+    var color = [null, Color.WHITE, Color.YELLOW, Color.BLUE];
 
     var endFunc = [
 
@@ -356,7 +615,9 @@ function useItem(x, y, z, I, b) {
   } catch(e) {
     blackRoom.error(e);
   }
+
 }
+//blackRoom.download("https://raw.githubusercontent.com/Duduzzing/MCPE-ModPE-Script/master/Survival-Kit/resource/chest.png", "/chest.png");
 
 
 
@@ -376,6 +637,4 @@ function useItem(x, y, z, I, b) {
 
 
 
-
-
-
+ 	
