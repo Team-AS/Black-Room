cbcdbd869958a0ecdfd3ef79b0dfb98ae9e0a550
Duduzzing <juhyung0109gmail.com>
2015-07-28 14:40:28 +0900

    added more headers

--- BlackRoom.js
+++ BlackRoom.js
@@ -57,6 +57,21 @@ var peAssets = PECTX.getAssets();
 var spritesheet = new BitmapFactory.decodeStream(peAssets.open("images/gui/spritesheet.png"));
 
 
+
+
+
+
+//false면 모든 기능이 마비되도록 할 예정
+//본 스크 제외 스크, 에드온, 모드 사용시 false
+
+var isScriptActivated = true;
+
+
+
+
+
+
+
 var blackRoom = {};
 
 
@@ -121,7 +136,7 @@ function callHeaderFile( headerPath ){
     var file = java.io.File(SDCARD + headerPath);
     
     if(!file.exists())
-      throw new Error(SDCARD+headerPath+" 경로에 파일이 존재하지 않습니다");
+      throw new Error(SDCARD+headerPath+" 경로에 헤더파일이 존재하지 않습니다");
     
     var fis = new java.io.FileInputStream(file);
     var isr = new java.io.InputStreamReader(fis);
@@ -153,8 +168,30 @@ function callHeaderFile( headerPath ){
 
 
 //makeToast
+//토스트 띄우는 함수
 
 eval( callHeaderFile( "/Git/BlackRoom/header/makeToast.txt" ) );
+
+
+//리소스 파일 다운로드 소스
+
+eval( callHeaderFile( "/Git/BlackRoom/header/download.txt" ) );
+
+
+//스크, 모드, 에드온 적용시 경고하는 소스
+
+eval( callHeaderFile( "/Git/BlackRoom/header/getEnabledPatches.txt" ) );
+
+
+//채팅관련 모든 함수
+
+eval( callHeaderFile( "/Git/BlackRoom/header/chat.txt" ) );
+
+
+
+
+
+
 
 
 
@@ -188,63 +225,6 @@ function ninepatch(file) {
 
 
 
-blackRoom.getEnabledScripts = function() {
-	
-var prefs=com.mojang.minecraftpe.MainActivity.currentMainActivity.get().getSharedPreferences("mcpelauncherprefs",0);
-
-var scripts=prefs.getString("enabledScripts","");
-
-return scripts.split(";");
-
-};
-
-blackRoom.getEnabledMods = function() {
-	
-var prefs=com.mojang.minecraftpe.MainActivity.currentMainActivity.get().getSharedPreferences("mcpelauncherprefs",0);
-
-var scripts=prefs.getString("enabledPatches","");
-
-return scripts.split(";");
-
-};
-
-blackRoom.getEnabledAddons = function() {
-	
-var prefs=com.mojang.minecraftpe.MainActivity.currentMainActivity.get().getSharedPreferences("mcpelauncherprefs",0);
-
-var scripts=prefs.getString("enabledAddons","");
-
-return scripts.split(";");
-
-};
-
-
-//false면 모든 기능이 마비되도록 할 예정
-
-var isScriptActivated = true;
-
-var scriptName = "BlackRoom.js";
-
-if(blackRoom.getEnabledMods().length > 0 || blackRoom.getEnabledAddons().length > 0 || blackRoom.getEnabledScripts()[0] != scriptName){
-	
-	 CTX.runOnUiThread(new java.lang.Runnable( {
-		run: function(){
-			try{    
-        var dialog = new android.app.AlertDialog.Builder(CTX);
-        dialog.setTitle("<Black Room> 본 스크립트 외 다른 스크립트, 모드, 애드온을 비활성화 시켜야 합니다");	        
-		     dialog.setNegativeButton("닫기", null); 
-		     dialog.create();
-		     dialog.show();
-		     
-		     } catch(e) {
-        blackRoom.error(e);        
-      }
-      }}));
-	
-isScriptActivated = false;	
-	
-} 
-
 
 /**
  * ClientMessage for BlackRoom script
@@ -260,148 +240,18 @@ blackRoom.message = function(message) {
 };
 
 
-/**
- * Download file from internet
- * 
- * String url
- * -the url that contains file to download
- * 
- * String path
- * -the path to download file (sdcard path included already)
- */
- 
-blackRoom.download = function(url, path, endFunc) {
-  new java.lang.Thread(new java.lang.Runnable({
-    run: function() {
-      try {
-        var file = new java.io.File(SDCARD, path);
-        if (file.exists()) {
-
-          return;
-
-        }
-
-        if (!file.getParentFile().exists()) {
-
-          file.getParentFile().mkdirs();
-
-        }
-
-        var URL = new java.net.URL(url);
-        var urlConn = URL.openConnection();
-        urlConn.connect();
-        var total = urlConn.getContentLength();
-        var bis = new java.io.BufferedInputStream(URL.openStream());
-        var bos = new java.io.BufferedOutputStream(new java.io.FileOutputStream(file));
-
-        var downloadDialog;
-
-        CTX.runOnUiThread(new java.lang.Runnable({
-          run: function() {
-            try {
-
-              downloadDialog = new android.app.ProgressDialog(CTX);
-
-              downloadDialog.setProgressStyle(android.app.ProgressDialog.STYLE_HORIZONTAL);
-              downloadDialog.setMax(total);
-              downloadDialog.setTitle("Resource file downloading");
-              downloadDialog.setMessage("downloading..");
-              downloadDialog.setCancelable(true);
-              downloadDialog.show();
-            } catch(e) {
-              blackRoom.error(e);
-            }
-
-          }
-        }));
-
-        var data = java.lang.reflect.Array.newInstance(java.lang.Byte.TYPE, 1024);
-        var count;
-        var downloadProgress = 0;
-
-        while ((count = bis.read(data)) != -1) {
-          java.lang.Thread.sleep(1);
-          bos.write(data, 0, count);
-          downloadProgress += count;
-
-          CTX.runOnUiThread(new java.lang.Runnable({
-            run: function() {
-              downloadDialog.setProgress(downloadProgress);
-            }
-          }));
-        }
-        urlConn.disconnect();
-        bos.flush();
-        bos.close();
-        bis.close();
-        CTX.runOnUiThread(new java.lang.Runnable({
-          run: function() {
-            try {
-              downloadDialog.dismiss();
-            } catch(e) {
-              blackRoom.error(e);
-            }
-          }
-        }));
-        
-        
-	 CTX.runOnUiThread(new java.lang.Runnable( {
-		run: function(){
-			try{    
-        var dialog = new android.app.AlertDialog.Builder(CTX);
-        dialog.setTitle("<Black Room> " +file.getName() + " 다운로드 완료" );	        
-		     dialog.setNegativeButton("나가기", null); 
-		     dialog.create();
-		     dialog.show();
-		     
-		     } catch(e) {
-        blackRoom.error(e);        
-      }
-      }}));
-      
-      
-      
-      } catch(e) {
-        blackRoom.error(e);
-      } finally{
-      	 if(endFunc != undefined)
-      endFunc();
-      	
-      	}
-    }
-  })).start();
-};
 
 
 
 
-blackRoom.download( "https://raw.githubusercontent.com/Team-AS/Black-Room/master/Zombie.png" ,"/Team-AS/Black-Room/Alex.png");
-
-blackRoom.download( "https://raw.githubusercontent.com/Team-AS/Black-Room/master/Skeleton.png" ,"/Team-AS/Black-Room/Steve.png");
-
-blackRoom.download( "https://raw.githubusercontent.com/Team-AS/Black-Room/master/minecraft.ttf","/Team-AS/Black-Room/minecraft.ttf");
-
-blackRoom.download( "https://raw.githubusercontent.com/Team-AS/Black-Room/master/talking.9.png","/Team-AS/Black-Room/talking.9.png");
-
-/*
-blackRoom.download(
-"https://github.com/Team-AS/Black-Room/raw/master/TestWorld.zip",
-"/Team-AS/Black-Room/testMap.zip",
-function(){
-	blackRoom.unZip(
-SDCARD+"/Team-AS/Black-Room/testMap.zip",
-SDCARD+"/games/com.mojang/minecraftWorlds");
-});
-
-*/
 
 /**
  * Extract the given file to the directory
  *
  * orginal authors affogatoman, ChalkPE
  *
- * @param {String|File} file - zip 파일의 경로 혹은 파일 객체
- * @param {String|File} target - 압축을 풀 폴더의 경로
+ * @param {String} file - zip 파일의 경로 혹은 파일 객체
+ * @param {String} target - 압축을 풀 폴더의 경로
  */
 
 blackRoom.unZip = function(file, target) {
@@ -527,29 +377,7 @@ blackRoom.error(e);
 
 
 
-/**
- * Get the face bitmap for chatting by name
- * 
- * String name
- * -the character's name
- * 
- * return bitmap
- */
 
-blackRoom.getFaceByName = function(name) {
-  var path = SDCARD + "/Team-AS/Black-Room/" + name + ".png";
-
-  if (java.io.File(path).exists()) {
-
-    return BitmapFactory.decodeFile(path);
-
-  } else {
-
-    throw new Error("No image exists in: " + path);
-
-  }
-
-};
 
 
 
@@ -628,7 +456,6 @@ blackRoom.vibrate = function(howLong) {
 
 /**
  * Vibrate screen
- * (testing)
  * 
  * int howLong
  * -set how long will the screen shake
@@ -700,315 +527,12 @@ blackRoom.error(e);
 
 };
 
-/**
- * Prepare PopupWindows for chat
- * 
- * return PopupWindow (obj)
- */
 
-blackRoom.prepareChat = function() {
 
-try{
-  
-  //this speed should be 0, but 10 for just now
-  ModPE.setGameSpeed(10);
-    
- var imageView1 = new ImageView(CTX);
 
-  var personWindow1 = new PopupWindow(imageView1, dp(260), dp(350) );
-  personWindow1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
 
-  personWindow1.showAtLocation(CTX.getWindow().getDecorView(), Gravity.LEFT | Gravity.CENTER, 0, -(dp(60)));
 
-  var imageView2 = new ImageView(CTX);
 
-  var personWindow2 = new PopupWindow(imageView2, dp(260), dp(350));
-  personWindow2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-
-  personWindow2.showAtLocation(CTX.getWindow().getDecorView(), Gravity.RIGHT | Gravity.CENTER, 0, -(dp(60)));
-  
-  var nWid = dp(200);
-  var nHei = dp(60);
-
-  var name1 = new Button(CTX);
-  
-  name1.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX,20);
-  
-  name1.setTextColor(Color.BLACK);
-
-  name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-  
-  var nameWindow1 = new PopupWindow(name1, nWid, nHei);
-  
-  nameWindow1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-  
-  nameWindow1.showAtLocation(CTX.getWindow().getDecorView(), Gravity.LEFT | Gravity.CENTER, 0, dp(70));
-
-  var name2 = new Button(CTX);
-  
-  name2.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX,20)
-  
-  name2.setTextColor(Color.BLACK);
-
-  name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-    
-  var nameWindow2 = new PopupWindow(name2, nWid, nHei);
-  
-  nameWindow2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-  
-  nameWindow2.showAtLocation(CTX.getWindow().getDecorView(), Gravity.RIGHT | Gravity.CENTER, 0, dp(70));
-
-  var screen = new Button(CTX);
-  
-  screen.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-
-
-
-
-  var talkWindow = new PopupWindow(screen,screenWidth, screenHeight / 3);
-
-  talkWindow.setBackgroundDrawable( ninepatch("/Team-AS/Black-Room/talking.9.png"));
-
-  talkWindow.showAtLocation(CTX.getWindow().getDecorView(), Gravity.CENTER | Gravity.BOTTOM, 0, 0);
-
-  
-  var arrowBitmap = new Bitmap.createScaledBitmap( new Bitmap.createBitmap(spritesheet,38,11,8,4), 32, 14, false);
-  
- var b  = new Button(CTX);
-
- b.setBackgroundDrawable(new Drawable.BitmapDrawable(arrowBitmap));
-
-
-  var arrowWindow = new PopupWindow(b,32,14);
-
-  arrowWindow.showAtLocation(CTX.getWindow().getDecorView(), Gravity.BOTTOM | Gravity.RIGHT, dp(20), dp(12));
-
-
-  var obj = {
-
-    talkWindow: talkWindow,
-    nameWindow1: nameWindow1,
-    nameWindow2: nameWindow2,
-    personWindow1: personWindow1,
-    personWindow2: personWindow2,
-    arrowWindow: arrowWindow
-
-  };
-
-  return obj;
-} catch(e) {
-	
-  blackRoom.error(e);	
-	
-}
-};
-
-/**
- * The Chatting function
- * 
- * Object talkWin
- * -prepareChat
- * 
- * String[] character
- * -defines which character is speaking (name)
- * 
- * Sring[] message
- * -the talking
- * 
- * Int[] point
- * -font size
- * 
- * Bool[] doVibrate
- * -true = virate when talk
- * 
- * Bool[] isLeft
- * -true = face is on the left side
- * 
- * function[]/null endFunc
- * -function that execute when the talking is done
- * 
- * int count
- * -the index for each arries
- * -put 0 when it's the beginning of chat
- */
-
-blackRoom.chat = function(talkWin, character, message, point, isLeft, color, doVibrate, endFunc, count) {
-
-  try {
-
-    var person1 = talkWin.personWindow1.getContentView();
-
-    var person2 = talkWin.personWindow2.getContentView();
-    
-    var name1 = talkWin.nameWindow1.getContentView();
-   
-    var name2 = talkWin.nameWindow2.getContentView();
-    
-    var letter = message[count].split("");
-    var currentChat = letter[0];
-    var letterCount = 0;
-    var isChatOver = false;
-
-    var screen = talkWin.talkWindow.getContentView();
-
-    screen.setText(currentChat);
-
-    screen.setGravity(Gravity.LEFT);
-
-    screen.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-
-    screen.setShadowLayer(1 / Math.pow(10, 10), 4, 4, Color.parseColor("#424142"));
-
-    screen.setTextSize(android.util.TypedValue.COMPLEX_UNIT_PX, point[count]);
-
-    screen.setTypeface(android.graphics.Typeface.createFromFile(FONT_PATH));
-
-    if (color[count] != null) {
-      screen.setTextColor(color[count]);
-    }
-    
-    var faceFile = blackRoom.getFaceByName(character[count]);
-
-    var face = new Bitmap.createScaledBitmap(
-    faceFile, dp(250), dp(350), false);
-
-    if (isLeft[count]) {
-    	
-    	 var matrix = new android.graphics.Matrix();
-        matrix.setScale(-1, 1);
-        
-        face = Bitmap.createBitmap(face, 0, 0, face.getWidth(), face.getHeight(), matrix, false);
-
-      person2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-
-      person1.setBackgroundDrawable(new Drawable.BitmapDrawable(face));
-      
-      name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-      
-      name2.setText("");
-      
-      name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.parseColor("#b0acad"))); 
-      
-      name1.setText(character[count]); 
-            
-
-    } else {
-
-      person1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-
-      person2.setBackgroundDrawable(new Drawable.BitmapDrawable(face));
-      
-      name1.setBackgroundDrawable(new Drawable.ColorDrawable(Color.TRANSPARENT));
-      
-      name1.setText("");
-            
-      name2.setBackgroundDrawable(new Drawable.ColorDrawable(Color.parseColor("#b0acad"))); 
-      
-      name2.setText(character[count]);
-
-    }
-
-    if (doVibrate[count]) {
-
-      blackRoom.vibrate(500);
-
-    }
-
-    var haveClicked = false;
-
-    new java.lang.Thread(new java.lang.Runnable({
-      run: function() {
-
-        try {
-
-          while (!haveClicked && !isChatOver && letterCount < letter.length - 1) {
-
-            java.lang.Thread.sleep(50);
-
-            letterCount++;
-
-            currentChat += letter[letterCount];
-
-            CTX.runOnUiThread(new java.lang.Runnable({
-              run: function() {
-
-                (!isChatOver) ? screen.setText(currentChat) : screen.setText(message[count]);
-              }
-            }));
-
-            if (letterCount == letter.length - 1) {
-
-              isChatOver = true;
-              haveClicked = true;
-
-            }
-
-          }
-
-        } catch(e) {
-          blackRoom.error(e);
-        }
-
-      }
-    })).start();
-
-    screen.setOnClickListener(new View.OnClickListener({
-      onClick: function(view) {
-        try {
-                	 
-          isChatOver = true;
-
-          screen.setText(message[count]);
-          
-          if (!haveClicked) {
-
-            haveClicked = true;
-            return;
-
-          }
-          
-          if (endFunc[count] != null) {
-
-            endFunc[count]();
-
-          }
-
-          count++;
-
-          if (character.length > count) {
-
-            blackRoom.chat(
-            talkWin, character, message, point, isLeft, color, doVibrate, endFunc, count);
-
-          } else {
-
-            ModPE.setGameSpeed(20);
-
-talkWin.personWindow1.dismiss();
-talkWin.personWindow2.dismiss();
-talkWin.nameWindow1.dismiss();
-talkWin.nameWindow2.dismiss();
-talkWin.talkWindow.dismiss();            
-talkWin.arrowWindow.dismiss();            
-
-            
-
-            isChatOver = false;
-
-          }
-
-        } catch(e) {
-          blackRoom.error(e);
-        }
-      }
-    }));
-
-  } catch(e) {
-
-    blackRoom.error(e);
-  }
-
-};
 
 function useItem(x, y, z, I, b) {
 
@@ -1067,8 +591,6 @@ if(I==260){
 blackRoom.setBrightness(0.1);
 }   
 
-if(I==261)
-hello();
 
 }
 
